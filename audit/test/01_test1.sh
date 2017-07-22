a9dccaa95e1555b1e8b2c24b2c90f2fc7ad1a436
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2017-07-23 00:15:51 +1000

    test - transfers working after enabled

--- audit/test/01_test1.sh
+++ audit/test/01_test1.sh
@@ -453,11 +453,11 @@ console.log("RESULT: ");
 
 
 // -----------------------------------------------------------------------------
-// Generate Tokens - Should not be possible 
+// Generate Tokens After Finalisation 
 // -----------------------------------------------------------------------------
-var generateTokensMessage = "Generate Tokens - Should not be possible";
+var generateTokensMessage = "Generate Tokens After Finalisation";
 console.log("RESULT: " + generateTokensMessage);
-var generateTokensTx = ph.generateTokens(account5, "1000000000000000000000000000", {from: contractOwnerAccount, gas: 2000000});
+var generateTokensTx = ph.generateTokens(account5, "1000000000000000000000000", {from: contractOwnerAccount, gas: 2000000});
 while (txpool.status.pending > 0) {
 }
 printTxData("generateTokensTx", generateTokensTx);
@@ -469,299 +469,69 @@ printTokenContractDetails();
 console.log("RESULT: ");
 
 
-exit;
-
-
-var name = "Feed";
-var symbol = "FEED";
-var initialSupply = 0;
-// NOTE: 8 or 18, does not make a difference in the tranches calculations
-var decimals = 18;
-var mintable = true;
-
-var minimumFundingGoal = new BigNumber(1000).shift(18);
-var cap = new BigNumber(2000).shift(18);
-
-#      0 to 15,000 ETH 10,000 FEED = 1 ETH
-# 15,000 to 28,000 ETH  9,000 FEED = 1 ETH
-var tranches = [ \
-  0, new BigNumber(1).shift(18).div(10000), \
-  new BigNumber(1500).shift(18), new BigNumber(1).shift(18).div(9000), \
-  cap, 0 \
-];
-
-var teamMembers = [ team1, team2, team3 ];
-var teamBonus = [150, 150, 150];
-
 // -----------------------------------------------------------------------------
-var cstMessage = "Deploy CrowdsaleToken Contract";
-console.log("RESULT: " + cstMessage);
-var cstContract = web3.eth.contract(cstAbi);
-console.log(JSON.stringify(cstContract));
-var cstTx = null;
-var cstAddress = null;
-
-var cst = cstContract.new(name, symbol, initialSupply, decimals, mintable, {from: contractOwnerAccount, data: cstBin, gas: 6000000},
-  function(e, contract) {
-    if (!e) {
-      if (!contract.address) {
-        cstTx = contract.transactionHash;
-      } else {
-        cstAddress = contract.address;
-        addAccount(cstAddress, "Token '" + symbol + "' '" + name + "'");
-        addTokenContractAddressAndAbi(cstAddress, cstAbi);
-        console.log("DATA: teAddress=" + cstAddress);
-      }
-    }
-  }
-);
-
-
+var cannotTransferMessage = "Cannot Move Tokens Before allowTransfers(...)";
 // -----------------------------------------------------------------------------
-var etpMessage = "Deploy PricingStrategy Contract";
-console.log("RESULT: " + etpMessage);
-var etpContract = web3.eth.contract(etpAbi);
-console.log(JSON.stringify(etpContract));
-var etpTx = null;
-var etpAddress = null;
-
-var etp = etpContract.new(tranches, {from: contractOwnerAccount, data: etpBin, gas: 6000000},
-  function(e, contract) {
-    if (!e) {
-      if (!contract.address) {
-        etpTx = contract.transactionHash;
-      } else {
-        etpAddress = contract.address;
-        addAccount(etpAddress, "PricingStrategy");
-        // addCstContractAddressAndAbi(etpAddress, etpAbi);
-        console.log("DATA: etpAddress=" + etpAddress);
-      }
-    }
-  }
-);
-
+console.log("RESULT: " + cannotTransferMessage);
+var cannotTransfer1Tx = apt.transfer(account6, "1000000000000", {from: account4, gas: 100000});
+var cannotTransfer2Tx = apt.approve(account7,  "30000000000000000", {from: account5, gas: 100000});
 while (txpool.status.pending > 0) {
 }
-
-printTxData("cstAddress=" + cstAddress, cstTx);
-printTxData("etpAddress=" + etpAddress, etpTx);
-printBalances();
-failIfGasEqualsGasUsed(cstTx, cstMessage);
-failIfGasEqualsGasUsed(etpTx, etpMessage);
-printTokenContractDetails();
-console.log("RESULT: ");
-
-
-// -----------------------------------------------------------------------------
-var mecMessage = "Deploy MintedEthCappedCrowdsale Contract";
-console.log("RESULT: " + mecMessage);
-var mecContract = web3.eth.contract(mecAbi);
-console.log(JSON.stringify(mecContract));
-var mecTx = null;
-var mecAddress = null;
-
-var mec = mecContract.new(cstAddress, etpAddress, multisig, $STARTTIME, $ENDTIME, minimumFundingGoal, cap, {from: contractOwnerAccount, data: mecBin, gas: 6000000},
-  function(e, contract) {
-    if (!e) {
-      if (!contract.address) {
-        mecTx = contract.transactionHash;
-      } else {
-        mecAddress = contract.address;
-        addAccount(mecAddress, "Crowdsale");
-        addCrowdsaleContractAddressAndAbi(mecAddress, mecAbi);
-        console.log("DATA: mecAddress=" + mecAddress);
-      }
-    }
-  }
-);
-
+var cannotTransfer3Tx = apt.transferFrom(account5, account7, "30000000000000000", {from: account7, gas: 200000});
 while (txpool.status.pending > 0) {
 }
-
-printTxData("mecAddress=" + mecAddress, mecTx);
+printTxData("cannotTransfer1Tx", cannotTransfer1Tx);
+printTxData("cannotTransfer2Tx", cannotTransfer2Tx);
+printTxData("cannotTransfer3Tx", cannotTransfer3Tx);
 printBalances();
-failIfGasEqualsGasUsed(mecTx, mecMessage);
+passIfGasEqualsGasUsed(cannotTransfer1Tx, cannotTransferMessage + " - transfer 0.000001 tokens ac4 -> ac6. CHECK no movement");
+passIfGasEqualsGasUsed(cannotTransfer2Tx, cannotTransferMessage + " - approve 0.03 tokens ac5 -> ac7");
+failIfGasEqualsGasUsed(cannotTransfer3Tx, cannotTransferMessage + " - transferFrom 0.03 tokens ac5 -> ac7. CHECK no movement");
 printCrowdsaleContractDetails();
+printPlaceHolderContractDetails();
+printTokenContractDetails();
 console.log("RESULT: ");
 
 
 // -----------------------------------------------------------------------------
-var bfaMessage = "Deploy BonusFinalizerAgent Contract";
-console.log("RESULT: " + bfaMessage);
-var bfaContract = web3.eth.contract(bfaAbi);
-console.log(JSON.stringify(bfaContract));
-var bfaTx = null;
-var bfaAddress = null;
-
-var bfa = bfaContract.new(cstAddress, mecAddress, teamBonus, teamMembers, {from: contractOwnerAccount, data: bfaBin, gas: 6000000},
-  function(e, contract) {
-    if (!e) {
-      if (!contract.address) {
-        bfaTx = contract.transactionHash;
-      } else {
-        bfaAddress = contract.address;
-        addAccount(bfaAddress, "BonusFinalizerAgent");
-        console.log("DATA: bfaAddress=" + bfaAddress);
-      }
-    }
-  }
-);
-
-while (txpool.status.pending > 0) {
-}
-
-printTxData("bfaAddress=" + bfaAddress, bfaTx);
-printBalances();
-failIfGasEqualsGasUsed(bfaTx, bfaMessage);
-console.log("RESULT: ");
-
-
+// Allow transfers
 // -----------------------------------------------------------------------------
-var invalidContribution1Message = "Send Invalid Contribution - 100 ETH From Account6 - Before Crowdsale Start";
-console.log("RESULT: " + invalidContribution1Message);
-var invalidContribution1Tx = eth.sendTransaction({from: account6, to: mecAddress, gas: 400000, value: web3.toWei("100", "ether")});
+var allowTransfersMessage = "Allow transfers";
+console.log("RESULT: " + generateTokensMessage);
+var allowTransfersTx = ph.allowTransfers(true, {from: contractOwnerAccount, gas: 2000000});
 while (txpool.status.pending > 0) {
 }
-printTxData("invalidContribution1Tx", invalidContribution1Tx);
+printTxData("allowTransfersTx", allowTransfersTx);
 printBalances();
-passIfGasEqualsGasUsed(invalidContribution1Tx, invalidContribution1Message);
+failIfGasEqualsGasUsed(allowTransfersTx, allowTransfersMessage);
 printCrowdsaleContractDetails();
+printPlaceHolderContractDetails();
 printTokenContractDetails();
 console.log("RESULT: ");
 
 
 // -----------------------------------------------------------------------------
-var stitchMessage = "Stitch Contracts Together";
-console.log("RESULT: " + stitchMessage);
-var stitch1Tx = cst.setMintAgent(mecAddress, true, {from: contractOwnerAccount, gas: 400000});
-var stitch2Tx = cst.setMintAgent(bfaAddress, true, {from: contractOwnerAccount, gas: 400000});
-var stitch3Tx = cst.setReleaseAgent(bfaAddress, {from: contractOwnerAccount, gas: 400000});
-var stitch4Tx = cst.setTransferAgent(mecAddress, true, {from: contractOwnerAccount, gas: 400000});
-var stitch5Tx = mec.setFinalizeAgent(bfaAddress, {from: contractOwnerAccount, gas: 400000});
+var canTransferMessage = "Can Move Tokens After allowTransfers(...)";
+// -----------------------------------------------------------------------------
+console.log("RESULT: " + canTransferMessage);
+var canTransfer1Tx = apt.transfer(account6, "1000000000000", {from: account4, gas: 100000});
+var canTransfer2Tx = apt.approve(account7,  "30000000000000000", {from: account5, gas: 100000});
 while (txpool.status.pending > 0) {
 }
-printTxData("stitch1Tx", stitch1Tx);
-printTxData("stitch2Tx", stitch2Tx);
-printTxData("stitch3Tx", stitch3Tx);
-printTxData("stitch4Tx", stitch4Tx);
-printTxData("stitch5Tx", stitch5Tx);
+var canTransfer3Tx = apt.transferFrom(account5, account7, "30000000000000000", {from: account7, gas: 200000});
+while (txpool.status.pending > 0) {
+}
+printTxData("canTransfer1Tx", canTransfer1Tx);
+printTxData("canTransfer2Tx", canTransfer2Tx);
+printTxData("canTransfer3Tx", canTransfer3Tx);
 printBalances();
-failIfGasEqualsGasUsed(stitch1Tx, stitchMessage + " 1");
-failIfGasEqualsGasUsed(stitch2Tx, stitchMessage + " 2");
-failIfGasEqualsGasUsed(stitch3Tx, stitchMessage + " 3");
-failIfGasEqualsGasUsed(stitch4Tx, stitchMessage + " 4");
-failIfGasEqualsGasUsed(stitch5Tx, stitchMessage + " 5");
+failIfGasEqualsGasUsed(canTransfer1Tx, canTransferMessage + " - transfer 0.000001 tokens ac4 -> ac6. CHECK for movement");
+failIfGasEqualsGasUsed(canTransfer2Tx, canTransferMessage + " - approve 0.03 tokens ac5 -> ac7");
+failIfGasEqualsGasUsed(canTransfer3Tx, canTransferMessage + " - transferFrom 0.03 tokens ac5 -> ac7. CHECK for movement");
 printCrowdsaleContractDetails();
+printPlaceHolderContractDetails();
 printTokenContractDetails();
 console.log("RESULT: ");
-
-
-// -----------------------------------------------------------------------------
-// Wait for crowdsale start
-// -----------------------------------------------------------------------------
-var startsAtTime = mec.startsAt();
-var startsAtTimeDate = new Date(startsAtTime * 1000);
-console.log("RESULT: Waiting until startAt date at " + startsAtTime + " " + startsAtTimeDate +
-  " currentDate=" + new Date());
-while ((new Date()).getTime() <= startsAtTimeDate.getTime()) {
-}
-console.log("RESULT: Waited until start date at " + startsAtTime + " " + startsAtTimeDate +
-  " currentDate=" + new Date());
-
-
-// -----------------------------------------------------------------------------
-var validContribution1Message = "Send Valid Contribution - 100 ETH From Account6 - After Crowdsale Start";
-console.log("RESULT: " + validContribution1Message);
-var validContribution1Tx = mec.investWithCustomerId(account6, 123, {from: account6, to: mecAddress, gas: 400000, value: web3.toWei("100", "ether")});
-
-while (txpool.status.pending > 0) {
-}
-printTxData("validContribution1Tx", validContribution1Tx);
-printBalances();
-failIfGasEqualsGasUsed(validContribution1Tx, validContribution1Message);
-printCrowdsaleContractDetails();
-printTokenContractDetails();
-console.log("RESULT: ");
-
-
-// -----------------------------------------------------------------------------
-var validContribution2Message = "Send Valid Contribution - 1900 ETH From Account7 - After Crowdsale Start";
-console.log("RESULT: " + validContribution1Message);
-var validContribution2Tx = mec.investWithCustomerId(account7, 124, {from: account7, to: mecAddress, gas: 400000, value: web3.toWei("1900", "ether")});
-
-while (txpool.status.pending > 0) {
-}
-printTxData("validContribution2Tx", validContribution2Tx);
-printBalances();
-failIfGasEqualsGasUsed(validContribution2Tx, validContribution2Message);
-printCrowdsaleContractDetails();
-printTokenContractDetails();
-console.log("RESULT: ");
-
-
-// -----------------------------------------------------------------------------
-var finaliseMessage = "Finalise Crowdsale";
-console.log("RESULT: " + finaliseMessage);
-var finaliseTx = mec.finalize({from: contractOwnerAccount, to: mecAddress, gas: 400000});
-while (txpool.status.pending > 0) {
-}
-printTxData("finaliseTx", finaliseTx);
-printBalances();
-failIfGasEqualsGasUsed(finaliseTx, finaliseMessage);
-printCrowdsaleContractDetails();
-printTokenContractDetails();
-console.log("RESULT: ");
-
-
-// -----------------------------------------------------------------------------
-var transfersMessage = "Testing token transfers";
-console.log("RESULT: " + transfersMessage);
-var transfers1Tx = cst.transfer(account8, "1000000000000000000", {from: account6, gas: 100000});
-var transfers2Tx = cst.approve(account9,  "2000000000000000000", {from: account7, gas: 100000});
-while (txpool.status.pending > 0) {
-}
-var transfers3Tx = cst.transferFrom(account7, account9, "2000000000000000000", {from: account9, gas: 100000});
-while (txpool.status.pending > 0) {
-}
-printTxData("transfers1Tx", transfers1Tx);
-printTxData("transfers2Tx", transfers2Tx);
-printTxData("transfers3Tx", transfers3Tx);
-printBalances();
-failIfGasEqualsGasUsed(transfers1Tx, transfersMessage + " - transfer 1 token ac6 -> ac8");
-failIfGasEqualsGasUsed(transfers2Tx, transfersMessage + " - approve 2 tokens ac7 -> ac9");
-failIfGasEqualsGasUsed(transfers3Tx, transfersMessage + " - transferFrom 2 tokens ac7 -> ac9");
-printTokenContractDetails();
-console.log("RESULT: ");
-
-
-// -----------------------------------------------------------------------------
-var invalidPaymentMessage = "Send invalid payment to token contract";
-console.log("RESULT: " + invalidPaymentMessage);
-var invalidPaymentTx = eth.sendTransaction({from: account7, to: cstAddress, gas: 400000, value: web3.toWei("123", "ether")});
-
-while (txpool.status.pending > 0) {
-}
-printTxData("invalidPaymentTx", invalidPaymentTx);
-printBalances();
-passIfGasEqualsGasUsed(invalidPaymentTx, invalidPaymentMessage);
-printTokenContractDetails();
-console.log("RESULT: ");
-
-
-exit;
-
-
-// -----------------------------------------------------------------------------
-// Wait for crowdsale end
-// -----------------------------------------------------------------------------
-var endsAtTime = mec.endsAt();
-var endsAtTimeDate = new Date(endsAtTime * 1000);
-console.log("RESULT: Waiting until startAt date at " + endsAtTime + " " + endsAtTimeDate +
-  " currentDate=" + new Date());
-while ((new Date()).getTime() <= endsAtTimeDate.getTime()) {
-}
-console.log("RESULT: Waited until start date at " + endsAtTime + " " + endsAtTimeDate +
-  " currentDate=" + new Date());
 
 EOF
 grep "DATA: " $TEST1OUTPUT | sed "s/DATA: //" > $DEPLOYMENTDATA
